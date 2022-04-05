5f6cf8ccba8c3e5017f84be6b2c6a7ef05b225ed
evgmolt <evg.molt@gmail.com>
2022-04-05 09:06:28 +0400

    Elasticsearch test application completed

---
+++ ElasticsearchTest/ElasticsearchTest/ElasticConfig.cs
@@ -0,0 +1,35 @@
+﻿using Elasticsearch.Net;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticsearchTest
+{
+    internal static class ElasticConfig
+    {
+        private static IElasticClient _client;
+        static ElasticConfig()
+        {
+            var node1 = new Uri("http://localhost:9200/");
+            var node2 = new Uri("http://localhost:9300/");
+            var nodes = new Uri[]
+            {
+                node1
+               //,node2
+            };
+            var connectionPool = new SniffingConnectionPool(nodes);
+            var connectionSettings = new ConnectionSettings(connectionPool)
+                                .SniffOnConnectionFault(false)
+                                .SniffOnStartup(false)
+                                .SniffLifeSpan(TimeSpan.FromMinutes(1));
+            _client = new ElasticClient(connectionSettings);
+        }
+        public static IElasticClient GetClient()
+        {
+            return _client;
+        }
+    }
+}
---
+++ ElasticsearchTest/ElasticsearchTest/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElasticsearchTest.Models
+{
+    internal class Car
+    {
+        public int? CarID { get; set; }
+        public string Maker { get; set; }
+        public string Color { get; set; }
+        public Double? Price { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
